11b5f29e6c8839987a0b76286c62590467573f94
Sylvia Jitti <sylviajitti@gmail.com>
2022-09-08 20:29:33 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+/**
+ * main - Prints the sixe of many var types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of achar: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size f a float: %d byte(s)\n", sizeof(e));
+	return(0);
+}
+
