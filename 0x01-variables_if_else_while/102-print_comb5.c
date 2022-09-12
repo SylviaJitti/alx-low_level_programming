906e2c8959ee0a548cd3b47db65eb29f72477929
Sylvia Jitti <sylviajitti@gmail.com>
2022-09-12 15:34:43 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,10 +9,10 @@ int main(void)
 	int f_n = 0;
 	int s_n;
 
-	while (f_n <= 100)
+	while (f_n <= 98)
 	{
 		f_n = s_n;
-		while (s_n <= 100)
+		while (s_n <= 99)
 		{
 			if (s_n != f_n)
 			{
@@ -22,7 +22,7 @@ int main(void)
 				putchar((s_n / 10) + 48);
 				putchar((s_n % 10) + 48);
 
-				if (f_n != 98 || s_n != 98)
+				if (f_n != 98 || s_n != 99)
 				{
 					putchar(',');
 					putchar(' ');
@@ -32,6 +32,6 @@ int main(void)
 		}
 		f_n++;
 	}
-	putchar ('\n');
-	return (0);
+putchar ('\n');
+return (0);
 }
