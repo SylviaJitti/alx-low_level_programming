73b770cba765742617497f9fb522076bb88491df
Sylvia Jitti <sylviajitti@gmail.com>
2022-09-12 14:14:58 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all possible combinations of 3 digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+	int f_n;
+	int m_n;
+	int l_n;
+
+	while (x <= 99)
+	{
+		f_n = (x / 100 + '0');
+		m_n = (x / 10 % 10 + '0');
+		l_n = (c % 10 + '0');
+
+		if ((f_n < m_n) && (m_n < l_n))
+		{
+			putchar(f_n);
+			putchar(m_n);
+			putchar(l_n);
+
+			if (x != 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+putchar('\n');
+return (0);
+}
