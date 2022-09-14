70711c85f161aacba6878ed322b6c902a25a4661
Sylvia Jitti <sylviajitti@gmail.com>
2022-09-13 17:58:02 -0700

    Task one 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints abc
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+_putchar('\n');
+}
