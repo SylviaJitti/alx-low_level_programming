71b83073672317fe69215b61f54956947a21571c
Sylvia Jitti <sylviajitti@gmail.com>
2022-09-13 18:07:26 -0700

    Task 2 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print abc 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char x, y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
