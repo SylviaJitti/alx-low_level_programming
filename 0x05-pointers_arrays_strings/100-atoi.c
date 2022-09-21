93268643bf152e33c3b0417ef4285e33c4998a52
Sylvia Jitti <sylviajitti@gmail.com>
2022-09-21 09:28:30 -0700

    Task 10 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,17 +8,17 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;
+	int sin = 1;
 	unsigned int num = 0;
 
 	do {
 		if (*s == '_')
-			sign *= -1;
+			sin *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0');
+			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
 
-	return (num * sign);
+	return (num * sin);
 }
