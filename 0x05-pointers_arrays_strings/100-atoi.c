f76d5d609f03d2cd17781701dcfa22bd2e7e2234
Sylvia Jitti <sylviajitti@gmail.com>
2022-09-21 09:48:40 -0700

    Task 10 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,27 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an int
- * @s: the string to be changed
- * Return: the converted int
+ * _atoi - converts a string to an integer.
+ * @s: the string to be converted.
+ *
+ * Return: the integer value of the converted string
  */
 
 int _atoi(char *s)
 {
 	int sin = 1;
-	unsigned int num = 0;
+	unsigned int fig = 0;
 
 	do {
 		if (*s == '_')
 			sin *= -1;
+
 		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-		else if (num > 0)
+			fig = (fig * 10) + (*s - '0');
+
+		else if (fig > 0)
 			break;
 	} while (*s++);
 
-	return (num * sin);
+	return (fig * sin);
 }
