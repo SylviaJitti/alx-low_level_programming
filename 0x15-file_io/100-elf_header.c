5274beba77694e431ce8ccbd70d2801f7e53a6da
SylviaJitti <72154164+SylviaJitti@users.noreply.github.com>
2022-11-01 07:56:29 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -101,7 +101,7 @@ void print_data(unsigned char *e_ident)
  * print_version - Prints the version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF version.
  */
-void print_version(unsigned char *e_ident) 
+void print_version(unsigned char *e_ident)
 {
 	printf("  Version:                           %d",
 	       e_ident[EI_VERSION]);
@@ -190,7 +190,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 		printf("NONE (None)\n");
 		break;
 	case ET_REL:
-		print("REL (Relocatable file)\n");
+		printf("REL (Relocatable file)\n");
 		break;
 	case ET_EXEC:
 		printf("EXEC (Executable file)\n");
@@ -239,7 +239,6 @@ void close_elf(int elf)
 {
 	if (close(elf) == -1)
 	{
-
 		dprintf(STDERR_FILENO,
 			"Error: Can't close fd %d\n", elf);
 		exit(98);
@@ -293,7 +292,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_osabi(header->e_ident);
 	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
-	print_entry(header->e_entr, header->e_ident);
+	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
 	close_elf(fd);
